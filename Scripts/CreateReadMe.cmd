56290f0dbf49de0534e22ce2af95126899ab1170
Daniel Patterson <danielanywhere@users.noreply.github.com>
2025-02-07 08:46:22 -0600

    Updates to README

--- Scripts/CreateReadMe.cmd
+++ Scripts/CreateReadMe.cmd
@@ -2,12 +2,10 @@
 :: Create the ReadMe.md file from Docs/ReadMe.docx.
 :: This command is meant to be run from within the Scripts folder.
 SET FAR=C:\OneDrive\Develop\Active\FindAndReplace\FindAndReplace\bin\Debug\net6.0\FindAndReplace.exe
+SET MEDIA=C:\OneDrive\Develop\Shared\ShopTools\Images
 SET SOURCE=..\Docs\ReadMe.docx
-SET TARGETHTML=..\Docs\README.html
-SET TARGETMARKDOWN=..\README.md
+SET TARGET=..\README.md
 SET PATTERN=ReadmePostProcessing.json
 
-:: Embedded resources are only created if output is HTML.
-PANDOC -t html --embed-resources=true "%SOURCE%" -o "%TARGETHTML%"
-PANDOC -t markdown_strict "%TARGETHTML%" -o "%TARGETMARKDOWN%"
-"%FAR%" /wait "/workingpath:..\Docs" "/files:%TARGETMARKDOWN%" "/patternfile:%PATTERN%"
+PANDOC -t markdown_strict --extract-media="%MEDIA%" "%SOURCE%" -o "%TARGET%"
+"%FAR%" /wait "/workingpath:..\Docs" "/files:%TARGET%" "/patternfile:%PATTERN%"
